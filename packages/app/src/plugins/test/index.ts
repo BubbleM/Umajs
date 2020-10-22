fd00a0a9d2903b15ea266fd4165e2c64e5035b20
BubbleM <bubblelm@163.com>
2020-10-22 13:07:04 +0800

    config:comment out parts of code

--- packages/app/src/plugins/test/index.ts
+++ packages/app/src/plugins/test/index.ts
@@ -21,25 +21,25 @@ export default (): TPlugin => {
             redirect(ctx: IContext, data: any) {
                 const { url, status } = data;
 
-                ctx.status = status;
+                // ctx.status = status;
 
-                return ctx.redirect(url);
+                // return ctx.redirect(url);
             },
         },
-        use: {
-            async handler(ctx: IContext, next: Function) {
-                console.log('use before');
-                await next();
-                console.log('use after');
-            }
-        },
-        method: {
-            type: RequestMethod.GET,
-            async handler(ctx: IContext, next: Function) {
-                console.log('method get before');
-                await next();
-                console.log('method get after');
-            }
-        }
+        // use: {
+        //     async handler(ctx: IContext, next: Function) {
+        //         console.log('use before');
+        //         await next();
+        //         console.log('use after');
+        //     }
+        // },
+        // method: {
+        //     type: RequestMethod.GET,
+        //     async handler(ctx: IContext, next: Function) {
+        //         console.log('method get before');
+        //         await next();
+        //         console.log('method get after');
+        //     }
+        // }
     };
 };
